45fee8c43ca32a93ed02afc77ee066533604e948
sunjing <404891954@qq.com>
2018-11-25 13:04:37 +0800

    react16: return type - string

--- src/magic.js
+++ src/magic.js
@@ -1,29 +1,7 @@
 import React, { Component } from "react";
-const TableComponent = props => {
-  return (
-    <table>
-      <tbody>
-        <tr>{props.children}</tr>
-      </tbody>
-    </table>
-  );
-};
 
-const TableColumns = props => {
-  return (
-    <React.Fragment>
-      <td>td A</td>
-      <td>td B</td>
-      <td>td C</td>
-    </React.Fragment>
-  );
-};
 export default class Magic extends Component {
   render() {
-    return (
-      <TableComponent>
-        <TableColumns />
-      </TableComponent>
-    );
+    return "Look ma, no spans!";
   }
 }
